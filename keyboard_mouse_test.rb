d033a86dc6ad309b53e295ead371cad58adb44c0
IvoDimitrov <ivopdimitrov@gmail.com>
2020-02-01 21:51:43 +0200

    Added keyboard_mouse_test.rb

---
+++ keyboard_mouse_test.rb
@@ -0,0 +1,22 @@
+# load in the webdriver gem to interact with Selenium
+require "selenium-webdriver"
+require "chromedriver-helper"
+
+# create a driver object
+driver = Selenium::WebDriver.for:chrome
+# open Web Page 
+driver.navigate.to "https://formy-project.herokuapp.com/keypress" 
+
+# find the field for full name and send keys to that name field
+name = driver.find_element(id: 'name')
+name.send_keys('examples of commonly actions that are automated in WebDriver test')
+
+# find the button called “button” and click it
+button = driver.find_element(id: 'button')
+# is the element visible on the page?
+button.displayed?
+button.click
+
+puts driver.title
+
+driver.quit
